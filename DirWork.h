615d15f83139b8d4698d19001c439cecde937710
Patte <patten010203@gmail.com>
2024-03-02 12:00:42 +0300

    Added widened "Welcome"

--- DirWork.h
+++ DirWork.h
@@ -7,14 +7,9 @@ std::string space2underscore(std::string text);
 std::string currentTime();
 
 int movingArrow(int ymin, int ymax, int cur, int key);
-void makeDirList(std::string folder, std::vector<std::string>& folderList);
-void folderChoice(int menuSize, std::vector<std::string> folders, int& cur, bool& agree, std::string& folder);
 void fileChoice(int begCoord, std::vector<std::string> folders, int& cur, std::string& filepath);
 void makeFilesList(std::string filepath, std::vector<std::string>& folderList);
 
 std::string findFile(std::string title);
-std::string findFolder(std::string title);
-
-bool dirAccess(const std::string& folder);
 
 #endif //DIRWORK
